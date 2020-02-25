8124095e9ac2be910196a72b94a374c9551e0553
SuperBaniya <hydrogennaman@gmail.com>
2020-02-25 12:26:34 +0530

    Update poker.py

--- poker.py
+++ poker.py
@@ -2,11 +2,18 @@ import random
 
 
 face = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-suit = ['C','S','D','H']
+suit = ['C', 'S', 'D', 'H']
+
 
 class card():
-        def __init__(self):
-            self.face=random.choice(face)
-            self.suit = random.choice(suit)
-obj=card()
-print(obj.face,obj.suit)
+    def __init__(self):
+        self.face = random.choice(face)
+        self.suit = random.choice(suit)
+
+
+class player(card):
+    playerCards = []
+
+    def __init__(self):
+        self.playerCards[0] = card()
+        self.playerCards[1] = card()
