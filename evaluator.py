1c001abb8c4b25a21788a22043f236e78a711d8f
SuperBaniya <hydrogennaman@gmail.com>
2020-02-27 20:26:49 +0530

    Update evaluator.py

--- evaluator.py
+++ evaluator.py
@@ -1,5 +1,6 @@
 from poker import *
 
 
-def evaluateCards(cards):
-    pass
+def evaluateCards(card1, card2, card3):
+    if card1.face == card2.face or card2.face == card3.face or card1.face == card3.face:
+        ans = 'pair'
